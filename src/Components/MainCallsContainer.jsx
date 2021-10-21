0fe5f39370b77fb68672ef56be1e9fb590bdbb64
Anton <golubin1990@gmail.com>
2021-10-21 18:25:00 -0400

    Fix the top nav bar

--- src/Components/MainCallsContainer.jsx
+++ src/Components/MainCallsContainer.jsx
@@ -15,13 +15,16 @@ export default function MainCallsContainer() {
   return (
     <>
     <div className="container-calls-1">
+      <div className="container-wrap">
+        <div className="container-absolute">
+        <ul className="container--tabs">
+	    	  <li className={"tab" + (state.active_tab === "All" ? " tabs--active" : "")} onClick = {(event) => clickHandler(event)} id = "All">All Calls</li>
+	    	  <li className={"tab" + (state.active_tab === "Active" ? " tabs--active" : "")} onClick = {(event) => clickHandler(event)} id = "Active">Active Calls</li>
+	    	  <li className={"tab" + (state.active_tab === "Archived" ? " tabs--active" : "")} onClick = {(event) => clickHandler(event)} id = "Archived">Archived Calls</li>
+	      </ul>
+        </div>
+      </div>  
       <div className="container-calls-2">
-	    <ul className="container--tabs">
-	    	<li className={"tab" + (state.active_tab === "All" ? " tabs--active" : "")} onClick = {(event) => clickHandler(event)} id = "All">All Calls</li>
-	    	<li className={"tab" + (state.active_tab === "Active" ? " tabs--active" : "")} onClick = {(event) => clickHandler(event)} id = "Active">Active Calls</li>
-	    	<li className={"tab" + (state.active_tab === "Archived" ? " tabs--active" : "")} onClick = {(event) => clickHandler(event)} id = "Archived">Archived Calls</li>
-	    </ul>
-
       <ListItems />
       </div>
     </div>
