44350c7a4cf5cb8b11968bb2a61095fdcb890034
Anton <golubin1990@gmail.com>
2021-10-21 17:24:56 -0400

    Add more different icons to the card. Update transitions and information shown on the card. Add separator for grouping by data

--- src/Components/ListItem.jsx
+++ src/Components/ListItem.jsx
@@ -1,17 +1,21 @@
 import React, {useState} from 'react'
 import './ListItem.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faPhoneAlt, faChevronDown, faVoicemail, faShareSquare, faUndo } from '@fortawesome/free-solid-svg-icons'
+import {faPhoneAlt, faChevronDown, faVoicemail, faShareSquare, faUndo, faPhoneSlash, faSignInAlt, faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
 import TimeAgo from 'react-timeago'
 
-export default function ListItem({call_type, created_at, direction, duration, from, id, is_archived, to, via, changeArchiveStatus}) {
+export default function ListItem({call_type, created_at, direction, duration, from, id, is_archived, to, via, changeArchiveStatus, separator}) {
   const [toggled, setToggled] = useState(false)
   const phone = <FontAwesomeIcon icon={faPhoneAlt} size="2x"/>
+  const phoneMiss = <FontAwesomeIcon icon={faPhoneSlash} size="2x"/>
   const voicemail = <FontAwesomeIcon icon={faVoicemail} size="2x"/>
   const chevron = <FontAwesomeIcon icon={faChevronDown} size="3x"/>
   const archive = <FontAwesomeIcon icon={faShareSquare} size="3x"/>
   const unarchive = <FontAwesomeIcon icon={faUndo} size="3x"/>
-
+  const missedIncoming = <FontAwesomeIcon icon={faSignInAlt} size="2x"/>
+  const missedOutcoming = <FontAwesomeIcon icon={faSignOutAlt} size="2x"/>
+  const months = [ "January", "February", "March", "April", "May", "June", 
+           "July", "August", "September", "October", "November", "December" ];
 
   //A helper function to make duration look good (break it into minutes/seconds)
   const durationToString = (duration) => {
@@ -33,20 +37,28 @@ export default function ListItem({call_type, created_at, direction, duration, fr
 
 
   return (
+    <>
+    {separator && <div className = "date-separator">Activity on {months[Number(created_at.slice(5,7))]} {created_at.slice(9,10)}, {created_at.slice(0,4)}</div>}
     <div className={"card" + (toggled ? " active" : "")}>
-	    <div className="content">
+	    <div className={"content" + (toggled ? " active" : "")}>
 		    <div className="imgBx">
           {call_type === "voicemail" && <div className = {"icon-main-voicemail"}>{voicemail}</div>}
-			    {call_type !== "voicemail" &&  <div className = {"icon-main" + (call_type === "missed" ? "-missed" : "")}>{phone}</div>}
+			    {call_type !== "voicemail" &&  <div className = {"icon-main" + (call_type === "missed" ? "-missed" : "")}>
+            {direction === "outbound" ? missedOutcoming : missedIncoming}{call_type === "missed" ? phoneMiss : phone}
+          </div>}
 		    </div>
-		  <h2>{from}<br /><span><TimeAgo date={created_at}/></span></h2>
+		  <h2>{from}<br />
+        <div className="clock">
+          At {created_at.slice(11, 13) > 12 ? 
+          (created_at.slice(11, 13) - 12) + created_at.slice(13, 16) + " p.m." : 
+          (Number(created_at.slice(11, 13)) + created_at.slice(13, 16) + " a.m.")}
+        </div>
+
+      </h2>
 	  </div>
 	<ul className="navigation">
 		<li>
     <span>Call Duration: </span><span>{durationToString(duration)}</span>
-		</li>
-		<li>
-    <span>Direction: </span><span>{direction ? direction : "Not Available"}</span>
 		</li>
 		<li>
 		<span>To: </span><span>{to ? to : "Not Available"}</span>
@@ -61,5 +73,6 @@ export default function ListItem({call_type, created_at, direction, duration, fr
 		{chevron}
 	</div>
 </div>
+  </>
   )
 }
